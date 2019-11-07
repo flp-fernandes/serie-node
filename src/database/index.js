5f399203be565ffba6c51c0858d2aa87763d2d1e
hey-felipao <hey.felipao@gmail.com>
2019-11-07 09:43:58 -0300

    first commit

---
+++ src/database/index.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/noderest', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+mongoose.Promise = global.Promise;
+
+module.exports = mongoose;
